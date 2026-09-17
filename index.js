aec7c55058968c7d93f472a292ecfc622e5724d8
tomasmazza68-cell <tomasmazza68@gmail.com>
2026-09-17 20:00:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ app.use(express.json());
 
 const TOKEN = 'mi_codigo_secreto_123';
 
+// Esta es la puerta que ya abrimos
 app.get('/webhook', (req, res) => {
   if (req.query['hub.verify_token'] === TOKEN) {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).send("Error de contraseña");
   }
+});
+
+// Este es el NUEVO buzón para recibir mensajes
+app.post('/webhook', (req, res) => {
+  console.log("¡Llegó un mensaje de WhatsApp!");
+  console.log(JSON.stringify(req.body, null, 2)); // Esto nos muestra el mensaje en la pantalla
+  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
